cd7c3da74f09d94b919e12d5d1d8a0f5b3ec5ef4
kijk2869 <mareowst@gmail.com>
2020-05-10 10:38:51 +0900

    Setup.py Encoding Error Fix

--- koreanbots/__init__.py
+++ koreanbots/__init__.py
@@ -1,3 +1,4 @@
+﻿#-*- coding: utf-8 -*-
 """
 KoreanBots API Wrapper
 ~~~~~~~~~~~~~~~~~~~
--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
+#-*- coding: utf-8 -*-
+
 from setuptools import setup
 import re, os
 
 version = ''
-with open('koreanbots/__init__.py') as f:
+with open('koreanbots/__init__.py', encoding='UTF8') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 path = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
 
 requirements = []
-with open(f'{path}/requirements.txt') as f:
+with open(f'{path}/requirements.txt', encoding='UTF8') as f:
   requirements = f.read().splitlines()
 
 if not version:
     raise RuntimeError('version is not defined')
 
 readme = ''
-with open(f'{path}/README.rst') as f:
+with open(f'{path}/README.rst', encoding='UTF8') as f:
     readme = f.read()
 
 setup(
