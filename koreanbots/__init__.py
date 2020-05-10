ab98a97c94d0faf2d3993de97c86b12d20a46008
kijk2869 <mareowst@gmail.com>
2020-05-10 10:40:05 +0900

    version_info not changed

--- koreanbots/__init__.py
+++ koreanbots/__init__.py
@@ -20,4 +20,4 @@ from .errors import *
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='final', serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=1, releaselevel='final', serial=0)
