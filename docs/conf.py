718040298f9dc0111fc7573e47256905de0a016e
kijk2869 <mareowst@gmail.com>
2020-05-20 11:36:40 +0900

    Minor Update

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'MARY (kijk2869)'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
