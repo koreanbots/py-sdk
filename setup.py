eadca228780436fec71b62040276d219e44cabce
매리 <mareowst@gmail.com>
2020-05-20 23:09:32 +0900

    Not Support 3.5

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     install_requires=requirements,
     python_requires='>=3.6',
     classifiers=[
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
