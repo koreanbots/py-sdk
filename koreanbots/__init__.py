fbb421f1ab9d2dc36a0dbcdd90b58c25a09427f7
kijk2869 <mareowst@gmail.com>
2020-09-09 11:47:41 +0900

    fix: version up

--- koreanbots/__init__.py
+++ koreanbots/__init__.py
@@ -1,4 +1,4 @@
-﻿#-*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
 KoreanBots API Wrapper
 ~~~~~~~~~~~~~~~~~~~
@@ -6,10 +6,10 @@ A Simple Python API wrapper for KoreanBots.
 :license: MIT, 자세한 내용은 LISENCE를 확인해주세요.
 """
 
-__title__ = 'koreanbots'
-__author__ = 'kijk2869'
-__lisence__ = 'MIT'
-__version__ = '0.2.2'
+__title__ = "koreanbots"
+__author__ = "kijk2869"
+__lisence__ = "MIT"
+__version__ = "0.2.3"
 
 from collections import namedtuple
 
@@ -18,6 +18,6 @@ from .http import HTTPClient
 from .model import Category, Library
 from .errors import *
 
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=0, minor=2, micro=2, releaselevel='final', serial=0)
+version_info = VersionInfo(major=0, minor=2, micro=2, releaselevel="final", serial=0)
