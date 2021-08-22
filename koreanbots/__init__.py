8c14bf4bab94ccfe371706e543a019c33bfd7eb7
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-23 07:27:10 +0900

    chore(koreanbots): bump up 1.1.1 to 1.1.2

--- koreanbots/__init__.py
+++ koreanbots/__init__.py
@@ -15,6 +15,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-versioninfo = VersionInfo(1, 1, 1, "final", 0)
+versioninfo = VersionInfo(1, 1, 2, "final", 0)
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
