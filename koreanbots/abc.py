2056c16efe4c7fbb1c23f6ef2976b6980960846a
Mary <mareowst@gmail.com>
2021-09-18 17:47:18 +0900

    Cleanup the code

--- koreanbots/abc.py
+++ koreanbots/abc.py
@@ -1,56 +1,63 @@
-import abc
+from abc import abstractmethod, ABCMeta
 from typing import Any, Dict, List, Optional
 
 
-class KoreanbotsABC(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
+class KoreanbotsABC(metaclass=ABCMeta):
+    @abstractmethod
     def __init__(self, **response_data: Any) -> None:
         self.response_data = response_data
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def code(self) -> int:
         return self.response_data.get("code", 0)
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def version(self) -> int:
         return self.response_data.get("version", 0)
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def data(self) -> Dict[str, Any]:
         return self.response_data.get("data", {})
 
 
-class DiscordABC(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
+class DiscordABC(metaclass=ABCMeta):
+    @abstractmethod
     async def close(self) -> None:
         raise NotImplementedError
 
-    @abc.abstractmethod
+    @abstractmethod
     async def wait_until_ready(self) -> None:
         raise NotImplementedError
 
-    @abc.abstractmethod
+    @abstractmethod
     def is_closed(self) -> bool:
         raise NotImplementedError
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def guilds(self) -> List["DiscordGuildABC"]:
         raise NotImplementedError
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def user(self) -> Optional["DiscordUserABC"]:
         raise NotImplementedError
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def shard_count(self) -> Optional[int]:
         raise NotImplementedError
 
 
-class DiscordUserABC(metaclass=abc.ABCMeta):
-    @abc.abstractproperty
+class DiscordUserABC(metaclass=ABCMeta):
+    @property
+    @abstractmethod
     def id(self) -> int:
         raise NotImplementedError
 
 
-class DiscordGuildABC(metaclass=abc.ABCMeta):
+class DiscordGuildABC(metaclass=ABCMeta):
     ...
