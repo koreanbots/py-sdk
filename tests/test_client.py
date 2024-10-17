c95174f4ba3d1df9b1b44b33184b4d851547f5ca
Ryu Juheon <saidbysolo@gmail.com>
2024-10-18 00:24:18 +0900

    test: 테스트 코드 3.0.0에 맞춰서 수정

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,9 +8,9 @@ from koreanbots.model import KoreanbotsBot, KoreanbotsUser
 async def test_botinfo(session: Koreanbots):
     response = await session.botinfo(653534001742741552)
     assert response.code == 200
-    assert response.name == "KODL"
-    assert isinstance(response.owners[0], KoreanbotsUser)
-    assert isinstance(response.owners[0].bots[0], str)
+    assert response.data.name == "KODL"
+    assert isinstance(response.data.owners[0], KoreanbotsUser)
+    assert isinstance(response.data.owners[0].bots[0], str)
 
 
 @pytest.mark.asyncio
@@ -18,8 +18,8 @@ async def test_get_user_info(session: Koreanbots):
     response = await session.userinfo(285185716240252929)
 
     assert response.code == 200
-    assert isinstance(response.bots[0], KoreanbotsBot)
-    assert isinstance(response.bots[0].owners[0], str)
+    assert isinstance(response.data.bots[0], KoreanbotsBot)
+    assert isinstance(response.data.bots[0].owners[0], str)
 
 
 @pytest.mark.asyncio
@@ -27,8 +27,7 @@ async def test_get_server_info(session: Koreanbots):
     response = await session.serverinfo(653083797763522580)
 
     assert response.code == 200
-    assert isinstance(response.bots[0], KoreanbotsBot)
-    assert isinstance(response.bots[0].owners[0], str)
+    assert isinstance(response.data.owner, KoreanbotsUser)
 
 
 @pytest.mark.asyncio
